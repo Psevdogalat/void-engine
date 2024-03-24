9d9cd47da61220b355a5557140b26933cae3201d
psevdogalat <psevdogalat@gmail.com>
2024-03-24 22:49:28 +0700

    feat: add init run adn free procedures in engine core

--- engine/core.cpp
+++ engine/core.cpp
@@ -1,53 +1,83 @@
 #include <engine.hpp>
+#include <platform.h>
+#include <exception>
+#include <cstdlib>
+#include <stdio.h>
+
+#include <debug.h>
 
 using namespace VoidEngine;
 
-Engine::Engine()
-{
-	if(instance != nullptr)
-		throw std::runtime_error("Engine: multyinstance deprecated");
+static const unsigned int mwndHeight = 400;
+static const unsigned int mwndWidth = 600;
 
-	instance = this;
-	init = true;
+PLFMHWND hMainWnd;
+PLFMHGC hGC;
+
+static void keyProcedure(PLFMHWND hWnd, const char key){
+	DEBUG("wnd %d key %c pressed\n", hWnd, key);
 };
 
-Engine::~Engine()
-{
-
+void drawProcedure(PLFMHWND hWnd){
+	//render(hGC);
 };
 
-int Engine::run(int argc, const char ** argv)
-{
-	init();
-	loop();
-	free();	
-	return 0;
-}; 
-
-int Engine::init()
-{
-	initPlatform();
-	initRenderer();
-	onInit();
-	return 0;
+static void mouseProcedure(PLFMHWND hWnd, unsigned int x, unsigned int y,
+	unsigned int keys){
+	DEBUG("wnd %d mouse procedure %d %d keys %d\n", hWnd, x, y, keys);
 };
 
-int Engine::free()
-{
-	onFree();
-	freeRenderer();
-	freePlatform();
-	return 0;
+static void reshapeProcedure(PLFMHWND hWnd, unsigned int width, 
+	unsigned int height){
+	plfmRedrawWindow(hMainWnd);
 };
 
-int Engine::loop()
-{
-	 return loopPlatform();
+static void closeProc(PLFMHWND hWnd ){
+	plfmPostQuitMessage(0);
 };
 
-Engine * Engine::getInstance()
-{
-	return instance;
+static void timerProcedure(PLFMHTIMER hTimer){
+	DEBUG("timer %d occure\n", hTimer);
+	plfmRedrawWindow(hMainWnd);
 };
 
+void VoidEngine::init(){
+	if(plfmInit() != PLFM_SUCCESS){
+		printf("platform init error");
+		exit(1);
+	}
+
+	if(plfmInitGL() != PLFM_SUCCESS){
+		DEBUG("platform graphic library init error");
+		exit(1);
+	}
+
+	hMainWnd = plfmCreateWindow(0, 0, mwndWidth, mwndHeight);
+	if(hMainWnd == PLFM_INVALID_HANDLE){
+		DEBUG("Can't create main window");
+		plfmFree();
+		exit(1);
+	};
+
+	plfmSetKeyProc(hMainWnd, keyProcedure);
+	plfmSetRenderProc(hMainWnd, drawProcedure);
+	plfmSetMouseProc(hMainWnd, mouseProcedure);
+	plfmSetReshapeProc(hMainWnd, reshapeProcedure);
+	plfmSetCloseProc(hMainWnd, closeProc);
+	plfmSetTimer(1000, timerProcedure);
+
+	//hGC = plfmGLCreateGC(hMainWnd);
+	//plfmGLMakeCurrent(hGC);
+	//plfmGLMakeCurrent(PLFM_NULL_HANDLE);
+
+	plfmShowWindow(hMainWnd);
+};
+
+void VoidEngine::free(){
+	plfmFree();
+};
+
+void VoidEngine::run(){
+	plfmLoop();
+};
 
